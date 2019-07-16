9f14024ec7e5b05c7e850cd5110fd1d1f989a7da
Tracycuizh <15915781169@139.com>
2019-07-16 11:14:27 +0800

    third commit

--- app/src/main/java/com/example/tracy_zh/searchviewtest/MainActivity.java
+++ app/src/main/java/com/example/tracy_zh/searchviewtest/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         int s = 123;
         float ssf= 1245;
+        String dfds= "dfadsf";
     }
 }
